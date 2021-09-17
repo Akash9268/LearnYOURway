b5ebecdcf01d7988f77a9d9dca1a866b1e46b8c0
AARAMBH KESHRI <aarambhkeshri@Apples-MacBook-Air.local>
2021-09-17 15:09:53 +0530

    Added login page for teacher and student

--- learnYOURway/accounts/urls.py
+++ learnYOURway/accounts/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
     path('register/',views.register,name="register"),
     path('teacher_register/',views.teacher_register.as_view(),name="teacher_sign_up"),
     path('student_register/',views.Student_register.as_view(),name="Student_sign_up"),
-
-
+    path('teacher_login/',views.teacher_login,name="teacher_login"),
+    path('student_login/',views.student_login,name="student_login"),
+    path('main_page/',views.main_page,name='main_page'),
 ]
--- learnYOURway/accounts/views.py
+++ learnYOURway/accounts/views.py
@@ -3,15 +3,20 @@ from django.contrib.auth import login
 from django.shortcuts import redirect
 from django.views.generic import CreateView
 from django.urls import reverse
-
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 from .models import User,Teacher,Student
 from .forms import TeacherSignupForm,StudentSignupForm
 # Create your views here.
 
 
-
 def register(request):
 	return render(request,'register.html')
+
+
+def main_page(request):
+	return render(request,'base.html')
 
 
 class teacher_register(CreateView):
@@ -31,6 +36,7 @@ class teacher_register(CreateView):
 	def get_success_url(self):
 		return reverse('register')
 
+
 class Student_register(CreateView):
 	model = User
 	form_class = StudentSignupForm
@@ -47,3 +53,45 @@ class Student_register(CreateView):
 		
 	def get_success_url(self):
 		return reverse('register')
+
+
+def teacher_login(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user.is_teacher is False:
+				messages.error(request,"Invalid username.")
+			elif user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("main_page")
+			else:
+				messages.error(request, "Invalid username or password.")
+		else:
+			messages.error(request, "Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="teacher_login.html", context={"login_form": form})
+
+
+def student_login(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user.is_student is False:
+				messages.error(request,"Invalid username.")
+			elif user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("main_page")
+			else:
+				messages.error(request, "Invalid username or password.")
+		else:
+			messages.error(request, "Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="student_login.html", context={"login_form": form})
